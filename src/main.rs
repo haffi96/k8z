ec972945102f55c5ccb8e46de9adf5de152ec195
Haffi Mazhar <haffimazhar96@gmail.com>
2023-07-14 18:18:06 +0100

    port forward cmd working

--- src/main.rs
+++ src/main.rs
@@ -2,14 +2,15 @@
 use clap::Parser;
 use core::fmt;
 use duct::cmd;
-use inquire::Select;
+use inquire::{Select, Text};
 use k8s_openapi::api::core::v1::Pod;
 use kube::{
     api::{Api, ListParams, ResourceExt},
     Client,
 };
 use std::{
-    io::{self, Write},
+    error::Error,
+    io::{self, BufRead, BufReader, Read, Write},
     process::{Command, Stdio},
 };
 use tracing::info;
@@ -41,6 +42,86 @@ impl fmt::Display for PodActions {
     }
 }
 
+fn prompt(name: &str) -> String {
+    let mut line = String::new();
+    print!("{}", name);
+    std::io::stdout().flush().unwrap();
+    std::io::stdin()
+        .read_line(&mut line)
+        .expect("Error: could not read line");
+    return line.trim().to_string();
+}
+
+fn pod_actions_handler(pod_choice: String) -> Result<(), Box<dyn Error>> {
+    let pod_actions = vec![
+        PodActions::Describe,
+        PodActions::Exec,
+        PodActions::PortForward,
+        PodActions::Delete,
+        PodActions::ClipboardCopy,
+    ];
+    let actions = Select::new("Pick an action to perform!", pod_actions).prompt();
+
+    match actions {
+        Ok(PodActions::Describe) => {
+            let cmd_args = vec!["-n", "default", "describe", "pod", &pod_choice];
+            let out = cmd("kubectl", cmd_args).stderr_null().read();
+            match out {
+                Ok(res) => println!("{}", res),
+                Err(e) => println!("feck"),
+            }
+        }
+
+        Ok(PodActions::Exec) => {
+            let cmd_args = vec!["-n", "default", "exec", "-it", &pod_choice, "/bin/bash"];
+            // let mut cmd = Command::new("kubectl")
+            //     .args(cmd_args)
+            //     .stdout(Stdio::piped())
+            //     .stderr(Stdio::piped())
+            //     .spawn()?;
+            //
+            // let mut stdout = cmd.stdout.take().unwrap();
+            // let mut stderr = cmd.stderr.take().unwrap();
+            //
+            // //write
+            // let stdin = std::io::stdin();
+            // let mut input: Vec<String> = Vec::new();
+            // let mut line = String::new();
+            //
+            // //read
+            // for line in BufReader::new(stdout).lines() {
+            //     println!("{}", line?);
+            // }
+        }
+
+        Ok(PodActions::PortForward) => {
+            info!("Selected PortForward to {}", pod_choice);
+            let port_mapping = Text::new("Define port mapping. local_port:container_port >> ")
+                .prompt()
+                .unwrap();
+            let cmd_args = vec!["-n", "default", "port-forward", &pod_choice, &port_mapping];
+            let mut cmd = Command::new("kubectl")
+                .args(cmd_args)
+                .stdout(Stdio::piped())
+                .spawn()?;
+
+            for line in BufReader::new(cmd.stdout.take().ok_or("stdout missing")?).lines() {
+                println!("{}", line?);
+            }
+            cmd.wait()?;
+        }
+
+        Ok(PodActions::Delete) => info!("Selected Delete for {}", pod_choice),
+
+        Ok(PodActions::ClipboardCopy) => {
+            info!("Selected ClipboardCopy for {}", pod_choice)
+        }
+
+        Err(_) => info!("err fuck"),
+    }
+    Ok(())
+}
+
 #[tokio::main]
 async fn main() -> anyhow::Result<()> {
     let args = Cli::parse();
@@ -62,133 +143,14 @@ async fn main() -> anyhow::Result<()> {
                 let ans = Select::new("Pick a Pod!", options_to_render).prompt();
 
                 match ans {
-                    Ok(choice) => {
-                        let pod_actions = vec![
-                            PodActions::Describe,
-                            PodActions::Exec,
-                            PodActions::PortForward,
-                            PodActions::Delete,
-                            PodActions::ClipboardCopy,
-                        ];
-                        let actions =
-                            Select::new("Pick an action to perform!", pod_actions).prompt();
-
-                        match actions {
-                            Ok(PodActions::Describe) => {
-                                let cmd_args = vec!["-n", "default", "describe", "pod", &choice];
-                                let out = cmd("kubectl", cmd_args).stderr_null().read();
-                                match out {
-                                    Ok(res) => println!("{}", res),
-                                    Err(e) => println!("feck"),
-                                }
-                            }
-                            Ok(PodActions::Exec) => {
-                                // FIXME: This is facked
-                                let cmd_args = vec![
-                                    "-n",
-                                    "default",
-                                    "exec",
-                                    &choice,
-                                    "-t",
-                                    "--tty",
-                                    "--stdin",
-                                    "--",
-                                    "/bin/bash",
-                                ];
-
-                                let mut child = Command::new("kubectl")
-                                    .args(cmd_args)
-                                    .stdin(Stdio::piped())
-                                    .stdout(Stdio::piped())
-                                    .stderr(Stdio::piped())
-                                    .spawn()
-                                    .expect("Failed to spawn child process");
-
-                                // .expect("sh command failed to start");
-                                // let out = cmd("kubectl", cmd_args)
-                                //     .stderr_capture()
-                                //     .stdout_capture()
-                                //     .run()
-                                //     .unwrap();
-                                // match out {
-                                //     Ok(res) => println!("{}", res),
-                                //     Err(e) => println!("feck"),
-                                // }
-                            }
-                            Ok(PodActions::PortForward) => {
-                                info!("Selected PortForward to {}", choice)
-                            }
-                            Ok(PodActions::Delete) => info!("Selected Delete for {}", choice),
-                            Ok(PodActions::ClipboardCopy) => {
-                                info!("Selected ClipboardCopy for {}", choice)
-                            }
-                            Err(_) => info!("err fuck"),
-                        }
-                    }
-                    Err(_) => println!("There was an error, try again!"),
-                }
+                    Ok(choice) => pod_actions_handler(choice),
+                    Err(_) => Ok(()),
+                };
             }
             _ => println!("Not implemented!"),
         },
         _ => println!("Not implemented!"),
     }
-
-    // Create Pod blog
-    // info!("Creating Pod instance blog");
-    // let p: Pod = serde_json::from_value(json!({
-    //     "apiVersion": "v1",
-    //     "kind": "Pod",
-    //     "metadata": { "name": "blog2" },
-    //     "spec": {
-    //         "containers": [{
-    //           "name": "blog",
-    //           "image": "clux/blog:0.1.0"
-    //         }],
-    //     }
-    // }))?;
-
-    // let pp = PostParams::default();
-    // match pods.create(&pp, &p).await {
-    //     Ok(o) => {
-    //         let name = o.name_any();
-    //         assert_eq!(p.name_any(), name);
-    //         info!("Created {}", name);
-    //     }
-    //     Err(kube::Error::Api(ae)) => assert_eq!(ae.code, 409), // if you skipped delete, for instance
-    //     Err(e) => return Err(e.into()),                        // any other case is probably bad
-    // }
-
-    // // Watch it phase for a few seconds
-    // let establish = await_condition(pods.clone(), "blog", is_pod_running());
-    // let _ = tokio::time::timeout(std::time::Duration::from_secs(15), establish).await?;
-
-    // // Replace its spec
-    // info!("Patch Pod blog");
-    // let patch = json!({
-    //     "metadata": {
-    //         "resourceVersion": p1cpy.resource_version(),
-    //     },
-    //     "spec": {
-    //         "activeDeadlineSeconds": 5
-    //     }
-    // });
-    // let patchparams = PatchParams::default();
-    // let p_patched = pods
-    //     .patch("blog", &patchparams, &Patch::Merge(&patch))
-    //     .await?;
-    // assert_eq!(p_patched.spec.unwrap().active_deadline_seconds, Some(5));
-
-    // let lp = ListParams::default().fields(&format!("metadata.name={}", "blog")); // only want results for our pod
-    // for p in pods.list(&lp).await? {
-    //     info!("Found Pod: {}", p.name_any());
-    // }
-
-    // // Delete it
-    // let dp = DeleteParams::default();
-    // pods.delete("blog", &dp).await?.map_left(|pdel| {
-    //     assert_eq!(pdel.name_any(), "blog");
-    //     info!("Deleting blog pod started: {:?}", pdel);
-    // });
 
     Ok(())
 }
